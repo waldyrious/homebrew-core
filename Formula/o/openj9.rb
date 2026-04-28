015e259f435f7ff432accb9531006c68114635ed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-28 20:57:58 +0000

    openj9 0.59.0

--- Formula/o/openj9.rb
+++ Formula/o/openj9.rb
@@ -1,8 +1,8 @@
 class Openj9 < Formula
   desc "High performance, scalable, Java virtual machine"
   homepage "https://www.eclipse.org/openj9/"
-  url "https://github.com/eclipse-openj9/openj9/archive/refs/tags/openj9-0.57.0.tar.gz"
-  sha256 "84650d5f8e623bec413b72e5486a40f0b3dcc71435fa4dfe5b9bba5bea4c398d"
+  url "https://github.com/eclipse-openj9/openj9/archive/refs/tags/openj9-0.59.0.tar.gz"
+  sha256 "35d959da212f2dbc442c059d250f2cbc63c716b31d16e30d4cef3c508731b99f"
   license any_of: [
     "EPL-2.0",
     "Apache-2.0",
@@ -65,8 +65,8 @@ class Openj9 < Formula
   end
 
   resource "omr" do
-    url "https://github.com/eclipse-openj9/openj9-omr/archive/refs/tags/openj9-0.57.0.tar.gz"
-    sha256 "7da11f270722c3d99570ef53af6dd84ae5ca865c7f9bc2982c4c415b7cb585c9"
+    url "https://github.com/eclipse-openj9/openj9-omr/archive/refs/tags/openj9-0.59.0.tar.gz"
+    sha256 "8bcc98595c72373844fcf0e5a58fa4a870c87591adec85c0105a940781e357d3"
 
     livecheck do
       formula :parent
